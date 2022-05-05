664c9cf46f4b6c383d7ebf03f1bf098d94110243
ws_27 <ws_27@jogroup.eu>
2022-05-05 13:28:23 +0200

    add fifo file and in 05_js function to hight order

--- javascript_base_studio/05_javascript/05_javascript.js
+++ javascript_base_studio/05_javascript/05_javascript.js
@@ -1,13 +1,30 @@
-function task(message) {
-    // emulate time consuming task
-    let n = 10000000000;
-    while (n > 0) {
-        n--;
-    }
-    console.log(message);
+// function task(message) {
+//     // emulate time consuming task
+//     let n = 10000000000;
+//     while (n > 0) {
+//         n--;
+//     }
+//     console.log(message);
+// }
+
+// console.log('Start script...');
+// task('Call an API');
+// console.log('Done!');
+
+
+// --------------------------------------------------------
+function compose(f1, f2) {
+    let composedFunction = function composedFunction(...args) {
+        return f1(f2(...args));
+    };
+    return composedFunction;
 }
 
-console.log('Start script...');
-task('Call an API');
-console.log('Done!');
-
+function split(string) {
+    return string.split(" ");
+}
+function count(array) {
+    return array.length;
+}
+var countWords = compose(count, split);
+console.log(countWords("funzione di ordine superiore"));
