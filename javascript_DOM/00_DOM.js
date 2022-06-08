1b628b098502067aa56c2e7b7aedf48979e82fe2
salvo720_linux <salvocelano91@gmail.com>
2022-06-08 19:13:35 +0200

    stopped at : Javascript HTML DOM elements , from link ( https://www.w3schools.com/js/js_htmldom_elements.asp  )

--- javascript_DOM/00_DOM.js
+++ javascript_DOM/00_DOM.js
@@ -139,5 +139,62 @@ function prova3() {
 
     //--------------------------------------------------------------------
     // Trovare gli oggetti HTML 
+    // il primo HTML DOM Livello 1 ( 1998 ) , ha definito 11 oggetti HTML ,raccolte di oggetti e propieta . Questi sono acora validi in HTML5. 
+    // Successivamente in HTML DOM Level 3sono stati aggiunti piu oggetti, raccolte e propieta .
+
+    // Propieta                                        Descrizione                                                                              DOM (Livello)
+    // document.anchors                                Ritoran tutti gli attributi <a> che hanno come attributo name                               1
+    // document.baseURI                                Ritorna l'URI di base assoluto del documento                                                3
+    // document.body                                   Ritorna l'elemento <body>                                                                   1
+    // document.cookie                                 Ritorna i cookie del documento                                                              1
+    // document.doctype                                Ritorna il Doc type del documento                                                           3
+    // document.documentElement                        Ritorna l'elemeto <html >                                                                   3
+    // document.documentMode                           Ritorna la modalita usta dal browser                                                        3
+    // document.documentURI                            Ritrona l'URI del documento                                                                 3
+    // document.domain                                 Ritorna il nome del dominio del server dei documenti                                        1
+    // document.embeds                                 Ritorna tutti gli elementi <embed>                                                          3
+    // document.forms                                  Ritorna tutti gli elementi <form>                                                           1
+    // document.head                                   Ritorna tutti gli elementi <head>                                                           3
+    // document.images                                 Ritorna tutti gli elementi <img>                                                            1
+    // document.implementation                         Ritorna le implementazioni DOM                                                              3
+    // document.inputEncoding                          Ritorna la codifica del documento (set di caratteri )                                       3
+    // document.lastModified                           Ritorna la data e ora in cui il documento e stato aggiornato                                3
+    // document.links                                  Ritorna tutti i <area> e <a> che hanno l'attributo href                                     1
+    // documents.readyState                            Ritorna lo stato ( di caricamento ) del documento                                           3
+    // document.referrer                               Ritorna l'URI del riferimento (il link del documento )                                      1
+    // document.scripts                                Ritorna tutti gli elementi <script>                                                         3
+    // document.strictErrorChecking                    Ritorna se viene applicato il controllo degli errori                                        3
+    // document.title                                  Ritorna il title del documento                                                              1
+    // document.url                                    Ritorna l'URL completo del documento                                                        1
+
+    //--------------------------------------------------------------------
+
+    window.console.log("document.anchors :", document.anchors);
+    window.console.log(" document.baseURI :", document.baseURI);
+    window.console.log("document.body :", document.body);
+    window.console.log("document.cookie :", document.cookie);
+    window.console.log("document.doctype :", document.doctype);
+    window.console.log("document.documentElement :", document.documentElement);
+    window.console.log("document.documentMode :", document.documentMode);
+    window.console.log("document.documentURI :", document.cookie);
+    window.console.log("document.domain :", document.domain);
+    window.console.log("document.embeds :", document.embeds);
+    window.console.log("document.forms :", document.forms);
+    window.console.log("document.head :", document.head);
+    window.console.log("document.images :", document.images);
+    window.console.log("document.implementation :", document.implementation);
+    window.console.log("document.inputEncoding :", document.inputEncoding);
+    window.console.log("document.lastModified :", document.lastModified);
+    window.console.log("document.links :", document.links);
+    window.console.log("document.readyState :", document.readyState);
+    window.console.log("document.referrer :", document.referrer);
+    window.console.log("document.scripts :", document.scripts);
+    window.console.log("document.strictErrorChecking :", document.strictErrorChecking);
+    window.console.log("document.title :", document.title);
+    window.console.log("document.url :", document.url);
+
+
+    //--------------------------------------------------------------------
+    // Elementi Javascript HTML DOM 
 
 }   
